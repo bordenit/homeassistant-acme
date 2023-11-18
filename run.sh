7bcd0ca79d7fc96a867bc4c16157f130b82a0e27
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2023-11-18 12:53:16 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -22,6 +22,6 @@ while true
     ./acme.sh --install --create-account-key -m "${EMAIL}"
     ./acme.sh --set-default-ca --server zerossl
     ./acme.sh --register-account -m "${EMAIL}"
-    ./acme.sh --key-file "/acmessl/key.pem" --cert-file "/acmessl/cert.pem" --issue --dns "${DNSAPI}" -d "${DOMAIN}" --debug
+    ./acme.sh --key-file "/ssl/key.pem" --cert-file "/ssl/cert.pem" --issue --dns "${DNSAPI}" -d "${DOMAIN}" --debug
      sleep 1D
   done
