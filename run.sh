136ca5f2b6c175761cc0c1d54d1a707056631b95
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2023-11-18 20:48:47 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,25 +4,23 @@ API_SECRET=$(bashio::config 'acme_api_secret')
 DNSAPI=$(bashio::config 'acme_dnsapi')
 EMAIL=$(bashio::config 'acme_email')
 DOMAIN=$(bashio::config 'acme_domain')
-LE_WORKING_DIR="/ssl/acme"
+GD_Key="${API_KEY}"
+GD_Secret="${API_SECRET}"
+
 apk add openssl
 apk add socat
 apk add git
 apk add curl
 apk add wget
 
-mkdir -p /ssl/acme
+git clone "https://github.com/acmesh-official/acme.sh.git"
+cd acme.sh
+./acme.sh --install --create-account-key -m "${EMAIL}"
+./acme.sh --set-default-ca --server zerossl
+./acme.sh --register-account -m "${EMAIL}"
+cd acme.sh
 while true
   do
-    export GD_Key="${API_KEY}"
-    export GD_Secret="${API_SECRET}"
-    rm -rf acme.sh
-    rm -rf /root/.acme.sh
-    git clone "https://github.com/acmesh-official/acme.sh.git"
-    cd acme.sh
-    ./acme.sh --install --create-account-key -m "${EMAIL}"
-    ./acme.sh --set-default-ca --server zerossl
-    ./acme.sh --register-account -m "${EMAIL}"
     ./acme.sh --key-file "/ssl/key.pem" --cert-file "/ssl/cert.pem" --issue --dns "${DNSAPI}" -d "${DOMAIN}" --debug
      sleep 30d
   done
