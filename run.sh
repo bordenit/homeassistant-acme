4f4302420801965eabbe6162f2190900c8eadde1
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2023-11-19 07:49:28 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,19 +5,22 @@ DNSAPI=$(bashio::config 'acme_dnsapi')
 EMAIL=$(bashio::config 'acme_email')
 DOMAIN=$(bashio::config 'acme_domain')
 ISSUER=$(bashio::config 'acme_issuer')
+
 export GD_Key="${API_KEY}"
 export GD_Secret="${API_SECRET}"
 export LE_WORKING_DIR=/ssl/acme
+
 mkdir -p /ssl/acme
+
 git clone "https://github.com/acmesh-official/acme.sh.git"
-cd acme.sh
-./acme.sh --install --create-account-key -m "${EMAIL}"
-./acme.sh --set-default-ca --server "${ISSUER}"
-./acme.sh --register-account -m "${EMAIL}"
+
+./acme.sh/acme.sh --install --create-account-key -m "${EMAIL}"
+./acme.sh/acme.sh --set-default-ca --server "${ISSUER}"
+./acme.sh/acme.sh --register-account -m "${EMAIL}"
+
 function issue {
-    # Issue the certificate exit corretly if is not time to renew
     local RENEW_SKIP=2
-    ./acme.sh --key-file "/ssl/key.pem" --cert-file "/ssl/cert.pem" --issue -d ${DOMAIN} --dns ${DNSAPI} || { ret=$?; [ $ret -eq ${RENEW_SKIP} ] && return 0 || return $ret ;}
+    ./acme.sh/acme.sh --key-file "/ssl/key.pem" --cert-file "/ssl/cert.pem" --issue -d ${DOMAIN} --dns ${DNSAPI} || { ret=$?; [ $ret -eq ${RENEW_SKIP} ] && return 0 || return $ret ;}
 }
 
 while true
