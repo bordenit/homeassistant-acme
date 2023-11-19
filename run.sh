aba09eb780742ba2217aec55957c4e0ea087af50
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2023-11-18 22:53:18 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,8 +14,15 @@ cd acme.sh
 ./acme.sh --install --create-account-key -m "${EMAIL}"
 ./acme.sh --set-default-ca --server "${ISSUER}"
 ./acme.sh --register-account -m "${EMAIL}"
+function issue {
+    # Issue the certificate exit corretly if is not time to renew
+    local RENEW_SKIP=2
+    ./acme.sh --key-file "/ssl/key.pem" --cert-file "/ssl/cert.pem" --issue -d ${DOMAIN} --dns ${DNSAPI} || { ret=$?; [ $ret -eq ${RENEW_SKIP} ] && return 0 || return $ret ;}
+}
+
+issue
 while true
   do
-    ./acme.sh --key-file "/ssl/key.pem" --cert-file "/ssl/cert.pem" --issue --dns "${DNSAPI}" -d "${DOMAIN}"
+     issue
      sleep 24h
   done
