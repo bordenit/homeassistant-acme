bd67b480b070635e9d65e0e72516d2808d2faddc
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2024-06-18 06:08:00 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/with-contenv bashio
-API_KEY=$(bashio::config 'acme_api_key')
-API_SECRET=$(bashio::config 'acme_api_secret')
+GD_KEY=$(bashio::config 'acme_api_key')
+CF_Zone_ID=$(bashio::config 'acme_cf_zone_id')
+CF_Token=$(bashio::config 'acme_cf_cf_token')
+GD_SECRET=$(bashio::config 'acme_api_secret')
 DNSAPI=$(bashio::config 'acme_dnsapi')
 EMAIL=$(bashio::config 'acme_email')
 DOMAIN=$(bashio::config 'acme_domain')
 ISSUER=$(bashio::config 'acme_issuer')
 
-export GD_Key="${API_KEY}"
-export GD_Secret="${API_SECRET}"
-export LE_WORKING_DIR=/ssl/acme
+#If using GoDaddy
+export GD_Key="${GD_KEY}"
+export GD_Secret="${GD_SECRET}"
 
+#If using Cloudfare
+export CF_Token="${CF_Token}"
+export CF_Zone_ID="${CF_Zone_ID}"
+
+#Certificate directory
+export LE_WORKING_DIR=/ssl/acme
 mkdir -p /ssl/acme
 
 git clone "https://github.com/acmesh-official/acme.sh.git"
